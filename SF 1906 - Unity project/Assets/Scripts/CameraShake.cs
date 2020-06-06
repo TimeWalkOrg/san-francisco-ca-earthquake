ce2018ee8e7f16b7262c6646f7787a7671d3ca3d
tedbarnett <mail@tedbarnett.com>
2020-06-06 01:54:21 -0700

    Added earthquake rumble and shake simulation (starts at 10 seconds: settings in "TimeWalk Controller"

---
+++ SF 1906 - Unity project/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	public bool shaketrue = false;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (shaketrue)
+		{
+			if (shakeDuration > 0)
+			{
+				camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+				shakeDuration -= Time.deltaTime * decreaseFactor;
+			}
+			else
+			{
+				shakeDuration = 1f;
+				camTransform.localPosition = originalPos;
+				shaketrue = false;
+			}
+		}
+	}
+
+	public void shakecamera()
+	{
+		shaketrue = true;
+	}
+}
--- SF 1906 - Unity project/Assets/Scripts/TimeWalk.cs
+++ SF 1906 - Unity project/Assets/Scripts/TimeWalk.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class TimeWalk : MonoBehaviour
 {
+    public float earthquakeStartDelayInSeconds = 10.0f;
+    private bool earthquakeIsStarted = false;
+    public AudioSource earthquakeAudioClip;
+    public CameraShake cameraShake;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,12 @@ public class TimeWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float timeNow = Time.realtimeSinceStartup;
+        if(!earthquakeIsStarted && (timeNow> earthquakeStartDelayInSeconds))
+        {
+            cameraShake.shakecamera(); // starts the camera shaking effect
+            earthquakeAudioClip.Play(); // plays the sound
+            earthquakeIsStarted = true;
+        }
     }
 }
