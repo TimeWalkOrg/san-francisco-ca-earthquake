f7a222116153522b2b84204d966f6857a434fcb5
tedbarnett <mail@tedbarnett.com>
2020-06-11 18:20:05 -0700

    Added rubble and earthquake fallling objects.  Enabled real-time clock in GUI.

--- SF 1906 - Unity project/Assets/Scripts/TimeWalk.cs
+++ SF 1906 - Unity project/Assets/Scripts/TimeWalk.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -8,16 +10,29 @@ public class TimeWalk : MonoBehaviour
     public float earthquakeStartDelayInSeconds = 10.0f;
     private bool earthquakeIsStarted = false;
     public AudioSource earthquakeAudioClip;
+    public GameObject earthquakeObjects;
     public CameraShake cameraShake;
+    public TextMeshPro currentDateUIText;
+    public TextMeshPro currentTimeUIText;
+    private DateTime startClockTime;
+    public int startYear;
+    public int startMonth;
+    public int startDay;
+    public int startHour;
+    public int startMinute;
+    public int startSecond;
+    private TimeSpan timeDelta;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        startClockTime = new DateTime(startYear, startMonth, startDay, startHour, startMinute, startSecond);
+        DateTime startActualTime = DateTime.Now;
+        timeDelta = startClockTime - startActualTime;
+        string startDateString = startClockTime.ToString("MMMM dd, yyyy");
+        currentDateUIText.text = startDateString;
+        earthquakeObjects.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         float timeNow = Time.realtimeSinceStartup;
@@ -26,6 +41,28 @@ public class TimeWalk : MonoBehaviour
             cameraShake.shakecamera(); // starts the camera shaking effect
             earthquakeAudioClip.Play(); // plays the sound
             earthquakeIsStarted = true;
+            earthquakeObjects.gameObject.SetActive(true);
         }
+
+
+        // Clock time setting
+        System.DateTime time = DateTime.Now;
+        time = time + timeDelta;
+        int currentHour = time.Hour;
+        string appendTimeText = "am";
+        if(currentHour > 12)
+        {
+            currentHour = currentHour - 12;
+            appendTimeText = "pm";
+        }
+        string hour = time.Hour.ToString();
+        string minute = LeadingZero(time.Minute);
+        string second = LeadingZero(time.Second);
+        currentTimeUIText.text = hour + ":" + minute + ":" + second + " " + appendTimeText;
+    }
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+        //return n.ToString();
     }
 }
