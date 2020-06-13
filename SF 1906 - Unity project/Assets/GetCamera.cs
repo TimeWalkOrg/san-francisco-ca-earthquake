ff645baa9aa1dce2c6cefe55a60b38f6ebb46df4
Ted Barnett <mail@tedbarnett.com>
2020-06-13 13:16:52 -0700

    Working on moving HUD to FPS camera when prefab is activated.

---
+++ SF 1906 - Unity project/Assets/GetCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// GetCamera finds the main camera and makes it a child of the specified GameObject (the "cameraHolder")
+// Best to attach this script to the Player prefab (FPS or VR or whatever)
+
+public class GetCamera : MonoBehaviour
+{
+
+    public Camera PrefabCameraToActivate;
+
+    private Camera mainCamera;
+    private Canvas HUDCanvas;
+
+    void Start()
+    {
+
+    }
+
+    // When this prefab is instantiated (and "Awakes"), grab the camera
+    void Awake()
+    {
+        //This gets the Main Camera from the Scene
+        mainCamera = Camera.main;
+        //PrefabCameraToActivate.enabled = false; // disable player camera until prefab is "Awake"
+        //mainCamera.enabled = true;
+        HUDCanvas = GameObject.FindGameObjectWithTag("HUDCanvas").GetComponent<Canvas>();
+        Debug.Log("HUDCanvas.name = " + HUDCanvas.name);
+
+        EnablePlayerCamera();
+        MoveHUDToPlayerCamera();
+    }
+
+    void EnablePlayerCamera()
+    {
+        // mainCamera.enabled = false;
+        PrefabCameraToActivate.enabled = true;
+    }
+    void MoveHUDToPlayerCamera()
+    {
+        HUDCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+        HUDCanvas.worldCamera = PrefabCameraToActivate.GetComponent<Camera>();
+    }
+}
--- SF 1906 - Unity project/Assets/Normal/Examples/Cube Player/Scripts/FPSPlayerManager.cs
+++ SF 1906 - Unity project/Assets/Normal/Examples/Cube Player/Scripts/FPSPlayerManager.cs
@@ -4,7 +4,8 @@ using Normal.Realtime;
 namespace Normal.Realtime.Examples {
     public class FPSPlayerManager : MonoBehaviour {
         private Realtime _realtime;
-        public Transform startPosition;
+        public Transform startPosition; // camera object (will be grabbed by player prefab when instantiated)
+        public GameObject playerPrefab; // prefab of FPS or VR object
 
         private void Awake() {
             // Get the Realtime component on this game object
@@ -16,12 +17,14 @@ namespace Normal.Realtime.Examples {
 
         private void DidConnectToRoom(Realtime realtime) {
             // Instantiate the CubePlayer for this client once we've successfully connected to the room
-            Realtime.Instantiate("FPS 03 - A Walk Down Market Street 1906",                 // Prefab name
-                                position: startPosition.position,          // Start 1 meter in the air
-                                rotation: Quaternion.identity, // No rotation
-                           ownedByClient: true,                // Make sure the RealtimeView on this prefab is owned by this client
-                preventOwnershipTakeover: true,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
-                             useInstance: realtime);           // Use the instance of Realtime that fired the didConnectToRoom event.
+            // Make sure the prefab is saved in a folder named "Resources" (anywhere inside the Assets folder)
+            Debug.Log("playerPrefab.name = " + playerPrefab.name);
+            Realtime.Instantiate(playerPrefab.name,              // Prefab name
+                                position: startPosition.position,   // Set start position for player
+                                rotation: startPosition.rotation,   // Set start rotation (was Quaternion.identity)
+                           ownedByClient: true,                     // Make sure the RealtimeView on this prefab is owned by this client
+                preventOwnershipTakeover: true,                     // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
+                             useInstance: realtime);                // Use the instance of Realtime that fired the didConnectToRoom event.
         }
     }
 }
