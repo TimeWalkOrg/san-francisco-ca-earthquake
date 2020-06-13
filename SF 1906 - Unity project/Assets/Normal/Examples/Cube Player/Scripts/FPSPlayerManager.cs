2f5db164e33db4b9c5c5034a706c99f82d62e79d
Ted Barnett <mail@tedbarnett.com>
2020-06-13 00:25:44 -0700

    Enabling FPS multiplayer

--- SF 1906 - Unity project/Assets/Normal/Examples/Cube Player/Scripts/FPSPlayerManager.cs
+++ SF 1906 - Unity project/Assets/Normal/Examples/Cube Player/Scripts/FPSPlayerManager.cs
@@ -4,6 +4,7 @@ using Normal.Realtime;
 namespace Normal.Realtime.Examples {
     public class FPSPlayerManager : MonoBehaviour {
         private Realtime _realtime;
+        public Transform startPosition;
 
         private void Awake() {
             // Get the Realtime component on this game object
@@ -15,8 +16,8 @@ namespace Normal.Realtime.Examples {
 
         private void DidConnectToRoom(Realtime realtime) {
             // Instantiate the CubePlayer for this client once we've successfully connected to the room
-            Realtime.Instantiate("FPSPlayer",                 // Prefab name
-                                position: Vector3.up,          // Start 1 meter in the air
+            Realtime.Instantiate("FPS 03 - A Walk Down Market Street 1906",                 // Prefab name
+                                position: startPosition.position,          // Start 1 meter in the air
                                 rotation: Quaternion.identity, // No rotation
                            ownedByClient: true,                // Make sure the RealtimeView on this prefab is owned by this client
                 preventOwnershipTakeover: true,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
