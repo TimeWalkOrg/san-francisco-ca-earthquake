74a5c85cfa6f0dfc8765c0ec056ff195f2c553cc
SensoramaLab <gplay@sensoramalab.com>
2020-07-10 00:30:54 +0300

    multiview warning fix, locomotion fix

---
+++ SF 1906 - Unity project/Assets/Scripts/PlatformChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlatformChecker : MonoBehaviour
+{
+    public SC_FPSController fpsController;
+    void OnEnable()
+    {
+
+#if UNITY_STANDALONE || UNITY_EDITOR
+
+            fpsController.enabled = !isPresent();
+#endif
+
+    }
+
+    public bool isPresent()
+    {
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+        foreach (var xrDisplay in xrDisplaySubsystems)
+        {
+            if (xrDisplay.running)
+            {
+            Debug.Log("xr");
+                return true;
+            }
+        }
+        return false;
+    }
+}
