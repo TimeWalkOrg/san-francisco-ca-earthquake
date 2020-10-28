feff3c4a477f9cf2c6bd073faa75e21e18537d4b
Ted Barnett <mail@tedbarnett.com>
2020-10-27 21:29:23 -0400

    Running on Oculus Rift S via Windows Unity 2020.1.10f1.  No locomotion.

---
+++ SF 1906 - Unity project/Assets/Scripts/MainCameraOffAtStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraOffAtStart : MonoBehaviour
+{
+    public GameObject mainCamera;
+    private bool mainCameraOn = true;
+    void Update()
+    {
+        if(mainCameraOn)
+        {
+            if (GameObject.FindGameObjectsWithTag("Player").Length != 0)
+            {
+                mainCamera.SetActive(false);
+                mainCameraOn = false;
+            }
+            
+        }
+        
+    }
+
+}
